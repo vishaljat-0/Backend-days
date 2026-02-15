ed7be9dfacbad21b928fab0da1ad24dda8dc9edf
jatvishal57@gmail.com <jatvishal57@gmail.com>
2026-02-15 18:01:02 +0530

    imagekit link creation

--- day-14-insta_clone/src/controllers/post.controller.js
+++ day-14-insta_clone/src/controllers/post.controller.js
@@ -2,28 +2,48 @@ const postmodel = require("../models/post.model");
 const usermodel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
 const ImageKit = require("imagekit");
+let decoded=null
 
-
-
-
-const imagekit= new ImageKit({
-    privateKey:process.env.PRIVITE_KEY,
-    publicKey:process.env.PUBLIC_KEY,
-    urlEndpoint:process.env.URL
-   
-})
+const imagekit = new ImageKit({
+  privateKey: process.env.PRIVITE_KEY,
+  publicKey: process.env.PUBLIC_KEY,
+  urlEndpoint: process.env.URL,
+});
 
 let createpost = async (req, res) => {
-  const { caption, imageUrl, user } = req.body;
+  const { caption } = req.body;
 
   const file = await imagekit.upload({
-    file:req.file.buffer,
-    fileName:"test"
-  })
-  res.send(file)
+    file: req.file.buffer,
+    fileName: "test",
+    folder: "insta_clone",
+  });
 
-  console.log(req.body,req.file);
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({
+      message: "unauthorized access",
+    });
+  }
+  try {
+     decoded = jwt.verify(token, process.env.SECRET_KEY);
+  } catch (error) {
+    return res.status(401).json({
+      message: "unauthorized access",
+    });
+  }
+
+  const post = await postmodel.create({
+    caption: caption,
+    imageUrl: file.url,
+    user: decoded.id,
+  });
+  res.status(201).json({
+    message: "post created successfully",
+    post,
+  });
 };
+
 module.exports = {
   createpost,
 };
